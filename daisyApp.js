28f06094ec28ad2ab4c2a5ca84ae5f5106d0dfab
Daniel <dapascale@gmail.com>
2020-09-12 18:48:54 -0400

    header layout complete

--- daisyApp.js
+++ daisyApp.js
@@ -9,3 +9,13 @@ daisyApp.init = function(){
 $(function(){
     daisyApp.init();
 });
+
+
+// Ready the doc
+// Create a namespacing object for the App
+// Create an app initialize function
+// Create an array for the deck of cards
+// Create a way for cards placement to be randomized
+// Creat an event listener for the click on a card
+// If two cards match, they appear in sidebar
+// Else, flip cards back to face-down
