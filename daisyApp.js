e0cf92dbfea54f8901560c73f7f4fc544a102e62
Daniel <dapascale@gmail.com>
2020-09-14 18:58:06 -0400

    Primary game game functionality working. Finally resolved appending imgs properly

--- daisyApp.js
+++ daisyApp.js
@@ -7,56 +7,86 @@ const cardDeck = [
         name: 'Fox',
         imagePath: './assets/cardFox.jpg',
         altText: 'Cast member Fox from Daisy of Love',
-        id: 1,
+        id: 'fox',
         value: 1,
     },
     {
         name: 'Weasel',
         imagePath: './assets/cardWeasel.jpg',
         altText: 'Cast member Weasel from Daisy of Love',
-        id: 2,
+        id: 'weasel',
         value: 2,
     },
     {
         name: 'Torch',
         imagePath: './assets/cardTorch.jpg',
         altText: 'Cast member Torch from Daisy of Love',
-        id: 3,
+        id: 'torch',
         value: 3,
     },
     {
         name: 'Big Rig',
         imagePath: './assets/cardBigRig.jpg',
         altText: 'Cast member Big Rig from Daisy of Love',
-        id: 4,
+        id: 'bigRig',
         value: 4,
     },
 ]
 
 // duplicate the cards
-const allCards = cardDeck.concat(cardDeck);
+let allCards = cardDeck.concat(cardDeck);
+
+// randomize the Deck
+// NOTE*** that I found the below randomizer code on Stack Overflow
+allCards = allCards.sort(() => 0.5 - Math.random());
+
 
 // display the cards on the game board in a random order
 daisyApp.setGameArea = function(){
     // create a gameCard area
     $('#cardGame').append($(`<div>`).addClass('cardDeck'))
-    // loop through array 
+    // loop through array and grab image and alt text
     allCards.forEach((hunk) => {
-        // create a <div> for each item in deck array
-        const picture = hunk.imagePath;
-        const altText = hunk.altText;
-        // append an <li> with picture and alt text to the ul
-        $('.cardDeck').append($(`<div>`).addClass('card'));
-        $('.card').css('background-image', `url(${picture})`).css('background-size', 'cover').attr('alt', `${altText}`);
+        // create a <img> for each item in deck array
+        const cardImage = $('<img>').addClass('card').attr('src', hunk.imagePath).attr('alt', hunk.altText).attr('id', hunk.id)
+        // append a <div> with picture and alt text to the game are
+        $('.cardDeck').append(cardImage);
+    })
+    daisyApp.gameplay();
+}
+
+let count = 0;
+let oneFlip = '';
+let twoFlip = '';
+let oneFlipID = '';
+let twoFlipID = '';
+
+daisyApp.gameplay = function(){
+    // add event listen for click
+    $('.card').on('click', function(){
+        // only add class of show for up to two cards
+        if (count < 2) {
+            count++;
+            $(this).addClass('show');
+            // on first guess, assign the flipNumber, etc
+            if (count === 1) {
+                oneFlip = $(this);
+                oneFlipID = $(this).attr('id');
+            } else {
+                if (count === 2) {
+                    twoFlip = $(this)
+                    twoFlipID = $(this).attr('id');
+                }
+            }
+            if (oneFlipID === twoFlipID) {
+                $(oneFlip).addClass('match').attr('disabled');
+                $(twoFlip).addClass('match').attr('disabled');
+            }
+        }
     })
 }
 
-
-// randomize the cards on the board
-
-
 daisyApp.init = function(){
-    console.log(`we're back!`);
     daisyApp.setGameArea();
 }
 
