359f6737909f2fe5ef465d5f2fec7c6879443f7c
Daniel <dapascale@gmail.com>
2020-09-14 12:47:37 -0400

    totally revamped layout of the game to use divs instead of li items, switched to an all-Dom maniuplation FOR NOW

--- daisyApp.js
+++ daisyApp.js
@@ -1,20 +1,73 @@
+// Create a namespacing object for the App
 const daisyApp = {};
 
+// create an array to store the cards
+const cardDeck = [
+    {
+        name: 'Fox',
+        imagePath: './assets/cardFox.jpg',
+        altText: 'Cast member Fox from Daisy of Love',
+        id: 1,
+        value: 1,
+    },
+    {
+        name: 'Weasel',
+        imagePath: './assets/cardWeasel.jpg',
+        altText: 'Cast member Weasel from Daisy of Love',
+        id: 2,
+        value: 2,
+    },
+    {
+        name: 'Torch',
+        imagePath: './assets/cardTorch.jpg',
+        altText: 'Cast member Torch from Daisy of Love',
+        id: 3,
+        value: 3,
+    },
+    {
+        name: 'Big Rig',
+        imagePath: './assets/cardBigRig.jpg',
+        altText: 'Cast member Big Rig from Daisy of Love',
+        id: 4,
+        value: 4,
+    },
+]
+
+// duplicate the cards
+const allCards = cardDeck.concat(cardDeck);
+
+// display the cards on the game board in a random order
+daisyApp.setGameArea = function(){
+    // create a gameCard area
+    $('#cardGame').append($(`<div>`).addClass('cardDeck'))
+    // loop through array 
+    allCards.forEach((hunk) => {
+        // create a <div> for each item in deck array
+        const picture = hunk.imagePath;
+        const altText = hunk.altText;
+        // append an <li> with picture and alt text to the ul
+        $('.cardDeck').append($(`<div>`).addClass('card'));
+        $('.card').css('background-image', `url(${picture})`).css('background-size', 'cover').attr('alt', `${altText}`);
+    })
+}
+
+
+// randomize the cards on the board
 
 
 daisyApp.init = function(){
     console.log(`we're back!`);
+    daisyApp.setGameArea();
 }
 
-$(function(){
+// Ready the doc
+$(function () {
     daisyApp.init();
 });
 
 
-// Ready the doc
-// Create a namespacing object for the App
-// Create an app initialize function
-// Create an array for the deck of cards
+
+
 // Create a way for cards placement to be randomized
 // Creat an event listener for the click on a card
 // If two cards match, they appear in sidebar
